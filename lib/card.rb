a004e4d638c998167c36274fab81fc5324527a18
lewis <ljones140@gmail.com>
2015-10-07 16:33:14 +0100

    2nd test passing

--- lib/card.rb
+++ lib/card.rb
@@ -2,8 +2,12 @@ class Card
 
   attr_reader :on_journey
 
+  def initialize
+    @on_journey = false
+  end
+
   def touch_in(station)
     @on_journey = true
   end
-
+  
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,9 +1,17 @@
 require 'card'
 
 describe Card do
+
   let(:station){ double(:station, zone: :a) }
+
+  it 'starts not on journey' do
+    expect(subject.on_journey).to be false
+  end
+
+
   it 'can touch in' do
     subject.touch_in(station)
     expect(subject.on_journey).to be true
   end
+
 end
