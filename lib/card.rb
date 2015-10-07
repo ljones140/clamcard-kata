44dae9beef6d7c84fc254a628abe139cd4405e76
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-07 16:46:17 +0100

    Implemented zone a charging

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,10 @@
 class Card
 
-  attr_reader :on_journey, :start_zone
+  attr_reader :on_journey, :start_zone, :end_zone, :charge
 
   def initialize
     @on_journey = false
+    @charge = 0
   end
 
   def touch_in(station)
@@ -11,4 +12,13 @@ class Card
     @start_zone = station.zone
   end
 
+  def touch_out(station)
+    @on_journey = false
+    @end_zone = station.zone
+    calculate_charge
+  end
+
+  def calculate_charge
+    @charge = 2.50
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,6 +3,7 @@ require 'card'
 describe Card do
 
   let(:station){ double(:station, zone: :a) }
+  let(:out_station){ double(:out_station, zone: :a) }
 
   it 'starts not on journey' do
     expect(subject.on_journey).to be false
@@ -22,4 +23,18 @@ describe Card do
     expect(subject.start_zone).to be nil
   end
 
+  it 'can touch out' do
+    subject.touch_out(out_station)
+    expect(subject.on_journey).to be false
+  end
+
+  it 'knows touched out station zone' do
+    subject.touch_out(out_station)
+    expect(subject.end_zone).to eq(:a)
+  end
+
+  it 'can calculate the fare' do
+    subject.touch_out(out_station)
+    expect(subject.charge).to eq(2.50)
+  end
 end
